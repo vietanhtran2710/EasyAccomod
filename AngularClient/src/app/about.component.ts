dd31ad6f517c0ca59711cdde48dc51e21a294f5a
hanhpx <57087562+hanhpx@users.noreply.github.com>
2020-11-14 02:14:41 -0800

    Add files via upload

---
+++ AngularClient/src/app/about.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+    templateUrl: './about.component.html',
+    selector: 'app-about',
+    styleUrls: ['./about.component.scss']
+})
+
+export class AboutComponent{}
