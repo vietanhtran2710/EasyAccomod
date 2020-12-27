26aa126a87113a004380c4600d96ed5d45d75035
vietanhtran2710 <vietanhtran.uet@gmail.com>
2020-12-27 20:32:07 +0700

    Display post info along with notification

--- AngularClient/src/app/notification/notification.component.ts
+++ AngularClient/src/app/notification/notification.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService} from '../services/notification.service'
 import { AuthService } from '../services/auth.service';
+import { PostService} from '../services/post.service'
 
 @Component({
   selector: 'app-notification',
@@ -12,7 +13,10 @@ export class NotificationComponent implements OnInit {
   currentAccount
   notifications
 
-  constructor(private notficationService: NotificationService, private authService: AuthService) { }
+  postInfo
+
+  constructor(private notficationService: NotificationService, private authService: AuthService,
+              private postService: PostService) { }
 
   ngOnInit(): void {
     this.currentAccount = this.authService.currentUserValue;
@@ -22,6 +26,30 @@ export class NotificationComponent implements OnInit {
         this.notifications = data;
         console.log(this.notifications)
       });
+  }
+
+  reformatDate(dateTime) {
+    if (!dateTime) {
+      return ""
+    }
+    let dateTimeArr = dateTime.split("T")
+    let date = dateTimeArr[0].split("-")
+    let day = date[2]
+    let month = date[1]
+    let year = date[0]
+    let time = dateTimeArr[1].split(":")
+    let hour = time[0]
+    let minute = time[1]
+    let second = time[2].split(".")[0]
+    return `${day}-${month}-${year}`
+  }
+
+  getPostInfo(postID) {
+    this.postService.getPostsByQuery(`?postID=${postID}`)
+      .subscribe(data => {
+        this.postInfo = data[0];
+        console.log(this.postInfo)
+      })
   }
 
 }
